f1f16f5abeb01bda81cb4b94f4f36f4999403b47
JordiCorbilla <jordi.coll.corbilla@gmail.com>
2020-12-13 19:52:40 +0000

    adding sample output in markdown format

---
+++ console-table-runner/Program.cs
@@ -0,0 +1,54 @@
+﻿//MIT License
+
+//Copyright (c) 2020 Jordi Corbilla
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using console_table;
+
+namespace console_table_runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<TestClass>
+            {
+                new TestClass {Field1 = 321121, Field2 = "Hi 312321", Field3 = 2121.32m, Field4 = true, Field5 = new DateTime(1970, 1,1), Field6 = 34.43},
+                new TestClass {Field1 = 32321, Field2 = "Hi long text", Field3 = 21111111.32m, Field4 = true, Field5 = new DateTime(1970, 1,1), Field6 = 34.43},
+                new TestClass {Field1 = 321, Field2 = "Hi longer text", Field3 = 2121.32m, Field4 = true, Field5 = new DateTime(1970, 1,1), Field6 = 34.43},
+                new TestClass {Field1 = 13, Field2 = "Hi very long text", Field3 = 21111121.32m, Field4 = true, Field5 = new DateTime(1970, 1,1), Field6 = 34.43}
+            };
+
+            ConsoleTable<TestClass>.Initialize(list).Write();
+
+            //var test = new List<List<string>>
+            //{
+            //    new List<string>() {"AAA", "BBB", "CCC"},
+            //    new List<string>() {"AAA", "BBB", "CCC"},
+            //    new List<string>() {"AAA", "BBB", "CCC"},
+            //    new List<string>() {"AAA", "BBB", "CCC"}
+            //};
+
+            //ConsoleTable<List<string>>.Initialize(test).Write();
+        }
+    }
+}
---
+++ console-table/ConsoleTable.cs
@@ -0,0 +1,112 @@
+﻿//MIT License
+
+//Copyright (c) 2020 Jordi Corbilla
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace console_table
+{
+    public class ConsoleTable<T>
+    {
+        public List<string> PropertyNames { get; set; }
+        public Dictionary<string, int> MaxWidth { get; set; }
+        private List<T> Items { get; }
+
+        public static ConsoleTable<T> Initialize(List<T> list)
+        {
+            return new ConsoleTable<T>(list);
+        }
+        public ConsoleTable(List<T> list)
+        {
+            if (list.Count <= 0) return;
+            PropertyNames = new List<string>();
+            MaxWidth = new Dictionary<string, int>();
+            Items = list;
+            var properties = typeof(T).GetProperties();
+            foreach (var property in properties)
+            {
+                PropertyNames.Add(property.Name);
+                MaxWidth.Add(property.Name, property.Name.Length);
+            }
+
+            foreach (var row in Items)
+            {
+                foreach (var property in PropertyNames)
+                {
+                    var value = GetValue(row, property);
+                    if (!MaxWidth.ContainsKey(property))
+                    {
+                        MaxWidth.Add(property, value.Length);
+                    }
+                    else
+                    {
+                        if (value.Length > MaxWidth[property])
+                            MaxWidth[property] = value.Length;
+                    }
+                }
+            }
+        }
+
+        private static string GetValue(T item, string property)
+        {
+            var value = item.GetType().GetProperty(property)?.GetValue(item, null);
+            return value switch
+            {
+                string s => s,
+                int _ => value.ToString(),
+                bool _ => value.ToString(),
+                DateTime time => time.ToString("dd-MMM-yyyy"),
+                decimal value1 => value1.ToString("#,##0.00"),
+                double value1 => value1.ToString("#,##0.00"),
+                _ => (value != null ? value.ToString() : "")
+            };
+        }
+
+        public void Write()
+        {
+            if (Items.Count <= 0) return;
+            var s = "|";
+            foreach (var property in PropertyNames)
+            {
+                var length = MaxWidth[property] - property.Length;
+                s += $" {property}{new string(' ', length)} |";
+            }
+            Console.WriteLine(s);
+
+            s = PropertyNames.Aggregate("|", (current, property) => current + $" {new string('-', MaxWidth[property])} |");
+            Console.WriteLine(s);
+
+            foreach (var row in Items)
+            {
+                s = "|";
+                foreach (var property in PropertyNames)
+                {
+                    var value = GetValue(row, property);
+                    var length = MaxWidth[property] - value.Length;
+                    s += $" {value}{new string(' ', length)} |";
+                }
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
