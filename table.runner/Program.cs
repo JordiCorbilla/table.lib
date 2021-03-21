7148f8b2e040d9abeca1beec29005a141bb8e745
JordiCorbilla <jordi.coll.corbilla@gmail.com>
2021-03-21 09:45:44 +0000

    building samples section

--- table.runner/Program.cs
+++ table.runner/Program.cs
@@ -30,46 +30,9 @@ namespace table.runner
     {
         private static void Main()
         {
-            var list = new List<TestClass>
-            {
-                new TestClass
-                {
-                    Field1 = 321121, Field2 = "Hi 312321", Field3 = 2121.32m, Field4 = true,
-                    Field5 = new DateTime(1970, 1, 1), Field6 = 34.43
-                },
-                new TestClass
-                {
-                    Field1 = 32321, Field2 = "Hi long text", Field3 = 21111111.32m, Field4 = true,
-                    Field5 = new DateTime(1970, 1, 1), Field6 = 34.43
-                },
-                new TestClass
-                {
-                    Field1 = 321, Field2 = "Hi longer text", Field3 = 2121.32m, Field4 = true,
-                    Field5 = new DateTime(1970, 1, 1), Field6 = 34.43
-                },
-                new TestClass
-                {
-                    Field1 = 13, Field2 = "Hi very long text", Field3 = 21111121.32m, Field4 = true,
-                    Field5 = new DateTime(1970, 1, 1), Field6 = 34.43
-                },
-                new TestClass
-                {
-                    Field1 = 13, Field2 = "Hi very, long text", Field3 = 21111121.32m, Field4 = true,
-                    Field5 = new DateTime(1970, 1, 1), Field6 = 34.43
-                },
-                new TestClass
-                {
-                    Field1 = 13, Field2 = "Hi \"very\" long\n text", Field3 = 21111121.32m, Field4 = true,
-                    Field5 = new DateTime(1970, 1, 1), Field6 = 34.43
-                }
-            };
-
-            Table<TestClass>.Add(list).ToConsole();
-
-            Table<TestClass>.Add(list)
-                .HighlightValue(new HighlightOperator
-                    {Field = "Field3", Type = HighlightType.Decimal, DecimalValue = 2121.32m})
-                .ToConsole();
+            Samples.SimpleConsoleOutputForList();
+            Samples.SimpleConsoleOutputWithHighlighterForList();
+            
 
             var test = new List<IEnumerable<string>>
             {
@@ -118,13 +81,13 @@ namespace table.runner
                 .OverrideColumnsNames(new Dictionary<string, string> {{"Dynamic0", "ColumnA"}})
                 .FilterColumns(new[] {"Capacity", "Count"}).ToCsv(@"C:\temp\test.csv");
 
-            Table<TestClass>.Add(list).ToCsv(@"C:\temp\test-list.csv");
+            Samples.SimpleCsvOutputForList();
 
             Table<IEnumerable<string>>.Add(test)
                 .OverrideColumnsNames(new Dictionary<string, string> {{"Dynamic0", "ColumnA"}})
                 .FilterColumns(new[] {"Capacity", "Count"}).ToHtml(@"C:\temp\test.html");
 
-            Table<TestClass>.Add(list).ToHtml(@"C:\temp\test-list.html");
+            Samples.SimpleHtmlOutputForList();
 
             Table<IEnumerable<string>>.Add(test)
                 .OverrideColumnsNames(new Dictionary<string, string> {{"Dynamic0", "ColumnA"}})
---
+++ table.runner/Samples.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using table.lib;
+
+namespace table.runner
+{
+    public static class Samples
+    {
+        public static List<TestClass> GetSampleOutput()
+        {
+            var list = new List<TestClass>
+            {
+                new TestClass
+                {
+                    Field1 = 321121, Field2 = "Hi 312321", Field3 = 2121.32m, Field4 = true,
+                    Field5 = new DateTime(1970, 1, 1), Field6 = 34.43
+                },
+                new TestClass
+                {
+                    Field1 = 32321, Field2 = "Hi long text", Field3 = 21111111.32m, Field4 = true,
+                    Field5 = new DateTime(1970, 1, 1), Field6 = 34.43
+                },
+                new TestClass
+                {
+                    Field1 = 321, Field2 = "Hi longer text", Field3 = 2121.32m, Field4 = true,
+                    Field5 = new DateTime(1970, 1, 1), Field6 = 34.43
+                },
+                new TestClass
+                {
+                    Field1 = 13, Field2 = "Hi very long text", Field3 = 21111121.32m, Field4 = true,
+                    Field5 = new DateTime(1970, 1, 1), Field6 = 34.43
+                },
+                new TestClass
+                {
+                    Field1 = 13, Field2 = "Hi very, long text", Field3 = 21111121.32m, Field4 = true,
+                    Field5 = new DateTime(1970, 1, 1), Field6 = 34.43
+                },
+                new TestClass
+                {
+                    Field1 = 13, Field2 = "Hi \"very\" long\n text", Field3 = 21111121.32m, Field4 = true,
+                    Field5 = new DateTime(1970, 1, 1), Field6 = 34.43
+                }
+            };
+            return list;
+        }
+        public static void SimpleConsoleOutputForList()
+        {
+            Table<TestClass>.Add(GetSampleOutput()).ToConsole();
+        }
+
+        public static void SimpleConsoleOutputWithHighlighterForList()
+        {
+            Table<TestClass>.Add(GetSampleOutput())
+                .HighlightValue(new HighlightOperator
+                    { Field = "Field3", Type = HighlightType.Decimal, DecimalValue = 2121.32m })
+                .ToConsole();
+        }
+
+        public static void SimpleCsvOutputForList()
+        {
+            Table<TestClass>.Add(GetSampleOutput()).ToCsv(@"C:\temp\test-list.csv");
+        }
+
+        public static void SimpleHtmlOutputForList()
+        {
+            Table<TestClass>.Add(GetSampleOutput()).ToHtml(@"C:\temp\test-list.html");
+        }
+    }
+}
